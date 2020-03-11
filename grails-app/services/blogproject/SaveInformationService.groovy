86b260c7e466bcf64537f0a5a9cc341ac648f2fc
Gajendra <gajendra@nexthoughts.com>
2020-03-11 14:47:57 +0530

    add service

---
+++ grails-app/services/blogproject/SaveInformationService.groovy
@@ -0,0 +1,45 @@
+package blogproject
+
+import com.gajendra.Address
+import com.gajendra.User
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class SaveInformationService {
+
+    def serviceMethod() {
+
+    }
+    User user=new User()
+    Address address
+
+    User insertValue(String firstName, String lastName, String emailId, String mobileNumber, String occupation, String userName, String password)
+    {
+        user.firstName=firstName
+        user.lastName=lastName
+        user.emailId=emailId
+        user.mobileNumber=mobileNumber
+        user.occupation=occupation
+        user.userName=userName
+        user.password=password
+        return user
+    }
+
+    Address insertAddValue(List houseNumber=[], List roadName=[], List areaName=[], User user)
+    {   address=new Address()
+        for(int i=0;i<houseNumber.size();i++){
+            address=new Address()
+            address.houseNumber=houseNumber[i]
+            address.roadName=roadName[i]
+            address.areaName=areaName[i]
+            user.addToAddresses(address)
+
+            if (address.validate()) {
+                address.save(failOnError: true)
+
+            } else {
+                log.error("address  Error while saving Group Object $address")
+            }
+        }
+    }
+}
